9a3f133b6f3401f926e33bae574b0e317d410b4c
Le0n404rd <142341250+Le0n404rd@users.noreply.github.com>
2024-02-19 18:17:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ function toggleAnimation(button) {
   // Obtém a referência ao body, ao header e à imagem da logo
   const body = document.body;
   const header = document.querySelector('header');
+  const navi = document.querySelectorAll('li');
   const logoImage = document.querySelector('.image');
-  const buttons = document.querySelectorAll('button');
+  const buttons = document.querySelectorAll('.btn11');
   const spans = document.querySelectorAll('span');
   const titles = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
-  const githubIcon = document.querySelector('.bi-github');
-  const instagramIcon = document.querySelector('.bi-instagram');
+  const logo = document.querySelector('.logo');
+  const githubIcons = document.querySelectorAll('.btn-social button');
+  const instagramIcons = document.querySelectorAll('.btn-social button');
   const otherIcons = document.querySelectorAll('i:not(.bi-github):not(.bi-instagram)');
   const tituloParagraphs = document.querySelectorAll('p');
 
@@ -28,11 +30,21 @@ function toggleAnimation(button) {
     title.style.color = button.classList.contains('clicked') ? '#000000' : '#ffffff';
   });
 
-  // Altera a cor do ícone do GitHub
-  githubIcon.style.color = button.classList.contains('clicked') ? '#ffffff' : '#000000';
+  navi.forEach(item => {
+    item.style.borderColor = button.classList.contains('clicked') ? '#800000' : '#00FF08';
+  });
 
-  // Altera a cor do ícone do Instagram
-  instagramIcon.style.color = button.classList.contains('clicked') ? '#ffffff' : '#000000';
+  // Altera a cor dos ícones do GitHub
+  githubIcons.forEach(icon => {
+    icon.style.color = button.classList.contains('clicked') ? '#ffffff' : '#000000';
+    icon.style.backgroundColor = button.classList.contains('clicked') ? '#800000' : '#00FF08';
+  });
+
+  // Altera a cor dos ícones do Instagram
+  instagramIcons.forEach(icon => {
+    icon.style.color = button.classList.contains('clicked') ? '#ffffff' : '#000000';
+    icon.style.backgroundColor = button.classList.contains('clicked') ? '#800000' : '#00FF08';
+  });
 
   // Altera a cor dos outros ícones
   otherIcons.forEach(icon => {
@@ -49,11 +61,33 @@ function toggleAnimation(button) {
     // Se clicado, muda o tema para branco e atualiza a imagem da logo
     body.style.backgroundColor = '#ffffff'; // Branco
     header.style.backgroundColor = '#f2f2f2'; // Cinza claro
-    logoImage.style.backgroundImage = 'url(images/logograndeBRA.png)'; // Nova imagem da logo
+    logoImage.style.backgroundImage = 'url(images/logograndeBRA.png)';
+    logo.style.backgroundImage = 'url(images/logoVER.png)';
+
+    // Nova imagem da logo
   } else {
     // Se não clicado, retorna ao tema original e à imagem original da logo
     body.style.backgroundColor = '#000000'; // Preto
     header.style.backgroundColor = '#080707'; // Cor original do cabeçalho
-    logoImage.style.backgroundImage = 'url(images/logogrande.png)'; // Imagem original da logo
+    logoImage.style.backgroundImage = 'url(images/logogrande.png)';
+    logo.style.backgroundImage = 'url(images/logo.png)';
+    // Imagem original da logo
   }
 }
+
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
